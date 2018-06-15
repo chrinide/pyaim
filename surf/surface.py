5a306bbaba3216b890ee0266ab50b927e718ab39
Jose Luis Casals Sainz <jluiscasalssainz@gmail.com>
2018-06-15 13:00:02 +0200

    update

---
+++ surf/surface.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import time
+import numpy
+import ctypes
+import h5py
+
+from pyscf import lib
+from pyscf.lib import logger
+libdft = lib.load_library('libdft')
+libcgto = lib.load_library('libcgto')
+
+# For code compatiblity in python-2 and python-3
+if sys.version_info >= (3,):
+    unicode = str
+
+EPS = 1e-7
+OCCDROP = 1e-12
+GRADEPS = 1e-10
+RHOEPS = 1e-6
+MINSTEP = 1e-5
+MAXSTEP = 0.75
+SAFETY = 0.8
+ENLARGE = 1.2
+HMINIMAL = numpy.finfo(numpy.float64).eps
+LEBEDEV_NGRID = numpy.asarray((
+    1   , 6   , 14  , 26  , 38  , 50  , 74  , 86  , 110 , 146 ,
+    170 , 194 , 230 , 266 , 302 , 350 , 434 , 590 , 770 , 974 ,
+    1202, 1454, 1730, 2030, 2354, 2702, 3074, 3470, 3890, 4334,
+    4802, 5294, 5810))
+
+class BaderSurf(lib.StreamObject):
+
+    def __init__(self, datafile):
+        self.inuc = 0
+        self.epsiscp = 0.180
+        self.ntrial = 11
+        self.npang = 5810
+        self.epsroot = 1e-4
+        self.rmaxsurf = 10.0
+        self.rprimer = 0.4
+        self.backend = 'rkck'
+        self.epsilon = 1e-4 
+        self.step = 0.1
+        self.mstep = 100
+        self.surfile = 'surface.h5'
+        self.verbose = logger.NOTE
+        self.stdout = sys.stdout
+        self.max_memory = lib.param.MAX_MEMORY
+        self.chkfile = datafile
+        self.scratch = lib.param.TMPDIR 
+##################################################
+# don't modify the following attributes, they are not input options
+        self.mo_coeff = None
+        self.mo_occ = None
+        self.natm = None
+        self.coords = None
+        self.charges = None
+        self.xnuc = None
+        self.xyzrho = None
+        self.rpru = None
+        self.grids = None
+        self.rsurf = None
+        self.nlimsurf = None
+        self.rmin = None
+        self.rmax = None
+        self.nelectron = None
+        self.charge = None
+        self.spin = None
+        self.atm = None
+        self.bas = None
+        self.nbas = None
+        self.env = None
+        self.ao_loc = None
+        self.shls_slice = None
+        self.nao = None
+        self.non0tab = None
+        self.cart = None
+        self._keys = set(self.__dict__.keys())
+
+    def dump_input(self):
+
+        logger.info(self,'')
+        logger.info(self,'******** %s flags ********', self.__class__)
+        logger.info(self,'Verbose level %d' % self.verbose)
+        logger.info(self,'Scratch dir %s' % self.scratch)
+        logger.info(self,'Input data file %s' % self.chkfile)
+        logger.info(self,'Surface file %s' % self.surfile)
+        logger.info(self,'max_memory %d MB (current use %d MB)',
+                 self.max_memory, lib.current_memory()[0])
+        logger.info(self,'Surface for nuc %d' % self.inuc)
+        logger.info(self,'Nuclear coordinate %8.5f %8.5f %8.5f', *self.xnuc)
+        logger.info(self,'Rmaxsurface %8.5f' % self.rmaxsurf)
+        logger.info(self,'Ntrial %d' % self.ntrial)
+        logger.info(self,'Npang points %d' % self.npang)
+        logger.info(self,'Rprimer %8.5f' % self.rprimer)
+        logger.info(self,'Epsiscp %8.5f' % self.epsiscp)
+        logger.info(self,'Epsroot %8.5f' % self.epsroot)
+        logger.info(self,'ODE solver %s' % self.backend)
+        logger.info(self,'ODE tool %8.5f' % self.epsilon)
+        logger.info(self,'Max steps in ODE solver %d' % self.mstep)
+        logger.info(self,'Num atoms %d' % self.natm)
+        logger.info(self,'Num electrons %d' % self.nelectron)
+        logger.info(self,'Total charge %d' % self.charge)
+        logger.info(self,'Spin %d ' % self.spin)
+        logger.info(self,'Atom Coordinates (Bohr)')
+        for i in range(self.natm):
+            logger.info(self,'Nuclei %d with charge %d position : %8.5f  %8.5f  %8.5f', i, 
+                        self.charges[i], *self.coords[i])
+        return self
+
