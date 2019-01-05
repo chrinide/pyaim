59aaed7d06e7907942a1d2321d42459e8dd69d07
Jose Luis Casals Sainz <jluiscasalssainz@gmail.com>
2019-01-05 04:43:40 +0100

    Update

---
+++ example/scf_resume_dp.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.append('..')
+import tools
+
+natm = 3
+name = 'h2o.chk.h5'
+tools.print_txt(name, 0) 
+tools.print_txt(name, 1) 
+tools.print_txt(name, 2) 
+
